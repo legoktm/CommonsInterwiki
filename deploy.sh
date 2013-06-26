e4c14a6276c64e8b28c0b45bcd28257f0c88d41d
Kunal Mehta <legoktm@gmail.com>
2013-06-25 23:23:20 -0700

    shell script to quickly deploy changes with

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd ~/cgi-bin
+rm bootstrap.py
+wget https://raw.github.com/legoktm/CommonsInterwiki/master/bootstrap.py
+rm main.py
+wget https://raw.github.com/legoktm/CommonsInterwiki/master/main.py
+chmod +x main.py
